e2107647cd76106e521b2019f389713878a9ce46
mehmet <ciftcimehm@gmail.com>
2018-09-24 20:05:33 +0300

    first commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehmet
+ * Date: 23.09.2018
+ * Time: 22:12
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Hello Space!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug));
+    }
+}
